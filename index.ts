ce08ca6be9bbb2e9ead3fb42c94f1c3ed78ccb2e
adtam5 <adam.tamas@ingka.com>
2026-09-01 16:04:42 +0200

    add another log message

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ function main() {
   console.log("Running application...");
   console.log("Just another log message...");
   console.log("Hello");
+  console.log("This is a log message, foo bar.");
 }
 
 main();
