b69eef0c9b98a7762105daf0e38341261576f99c
adtam5 <adam.tamas@ingka.com>
2026-09-07 12:16:34 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ function main() {
   console.log("Running application...");
   console.log("Just another log message...");
   console.log("Hello");
-  console.log("This is a log message, foo bar.");
 }
 
 main();
